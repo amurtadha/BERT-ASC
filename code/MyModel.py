eb9e3e70924a9fba2802140fbcef7e4abbd1bc29
Murtadha Ahmed <38778295+amurtadha@users.noreply.github.com>
2024-06-27 17:04:41 +0800

    Update MyModel.py

--- code/MyModel.py
+++ code/MyModel.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from transformers import   AutoConfig, AutoModelForSequenceClassification
+
 from layers.modeling_custom import BertModel, BertPreTrainedModel, BertLayer, BertPooler
 
 class GRoIE(nn.Module):
@@ -55,3 +57,22 @@ class BERT_ASC(BertPreTrainedModel):
                 return attention, logits
             else:
                 return logits
+
+class BERT_ASC_vanila(nn.Module):
+
+    def __init__(self, args, hidden_size=256):
+        super(BERT_ASC_vanila, self).__init__()
+        config = AutoConfig.from_pretrained(args.pretrained_bert_name)
+        config.num_labels = args.lebel_dim
+        self.encoder = AutoModelForSequenceClassification.from_pretrained(args.pretrained_bert_name, config=config)
+        self.encoder.to('cuda')
+        # layers = [nn.Linear(config.hidden_size, hidden_size), nn.ReLU(), nn.Dropout(.3),
+        #           nn.Linear(hidden_size, args.lebel_dim)]
+
+        # layers = [nn.Linear(config.hidden_size, args.lebel_dim)]
+        # self.classifier = nn.Sequential(*layers)
+
+    def forward(self,  input_ids, token_type_ids=None, attention_mask=None, labels=None, return_attention=False):
+        outputs = self.encoder(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+
+        return outputs['logits']
